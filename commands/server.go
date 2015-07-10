7fe9e633171fef72cba923cebffb74dac147cc35
Russell Oliver <roliver8143@gmail.com>
2015-07-10 18:15:19 +1000

    Add basis for an inbuilt editor

--- commands/server.go
+++ commands/server.go
@@ -128,6 +128,13 @@ func serve(port int) {
 	if err != nil {
 		jww.ERROR.Fatalf("Invalid BaseURL: %s", err)
 	}
+
+	if WebEditor {
+		http.HandleFunc("/view/", makeHandler(viewHandler))
+		http.HandleFunc("/edit/", makeHandler(editHandler))
+		http.HandleFunc("/save/", makeHandler(saveHandler))
+	}
+
 	if u.Path == "" || u.Path == "/" {
 		http.Handle("/", fileserver)
 	} else {
